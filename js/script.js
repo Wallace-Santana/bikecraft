46b8d68be189bd8c47c8440f21a1d6d4f93e1efa
Wallace-Santana <wallace-santana@outlook.com.br>
2023-10-26 21:19:18 -0300

    producao

--- js/script.js
+++ js/script.js
@@ -1,97 +1,67 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-   const url = location.href;
-   const href = link.href;
-
- if (url.includes(href)) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
     link.classList.add("ativo");
- }
+  }
 }
 
 links.forEach(ativarLink);
 
+// Ativar Items do Orçamento
 
+const parametros = new URLSearchParams(location.search);
 
-// ativar items do orçamento 
-
-
-const parametros =  new URLSearchParams (location.search);
-
-function ativarProduto(parametro){
-    const elemento = document.getElementById(parametro);
-   
-    if(elemento){
-        elemento.checked = true;
-      
-    }
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 }
 
-parametros.forEach (ativarProduto)
+parametros.forEach(ativarProduto);
 
-
-
-//perguntas frequentes
-
-
-
+// Perguntas Frequentes
 const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta (event){
-    const pergunta = event.currentTarget;
-    const controls = pergunta.getAttribute("aria-controls");
-    const resposta = document.getElementById(controls);
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-    resposta.classList.toggle("ativa");  
-    const ativa = resposta.classList.contains("ativa");
-
-    pergunta.setAttribute("aria-expanded", ativa);
-
-     
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
-
-function eventosPerguntas (pergunta){
-    pergunta.addEventListener('click', ativarPergunta)
-
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
 
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-
-
-
-
-// Galeria de bike
-
-
-const galeria = document.querySelectorAll('.bicicleta-imagens img');
-const galeriaContainer = document.querySelector('.bicicleta-imagens');
-
-
-function trocarImagem(event){
-    const img = event.currentTarget;
-   const media =  matchMedia("(min-width: 1000px)").matches;
-
-    if (media) {
-galeriaContainer.prepend(img);
- }
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
 }
 
-function eventosGaleria(img){
-    img.addEventListener("click", trocarImagem);
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
 }
-
 
 galeria.forEach(eventosGaleria);
 
-
-
-//animação
-
-
-if(window.SimpleAnime){
-new SimpleAnime();
-
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
 }
